e402990d2b4a6b2b306fd1354c4d537b26a75df0
BRK2021 <rentalSkiBRK@interia.pl>
2025-01-09 11:37:02 +0100

    first commit

---
+++ src/main/java/com/project/ski_school/SkiSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.project.ski_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ski_school/SkiSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ski_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
